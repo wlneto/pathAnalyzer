fe583d6443ca7c8c536d9bab76874208ad895df4
Matheus Trevisan Moreira <matheus.trev@gmail.com>
2019-12-29 01:05:10 -0800

    Adding run script

---
+++ work/nnDesignSpace/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+nohup python neuralNetwork0.py > log0.txt &
+nohup python neuralNetwork1.py > log1.txt &
+nohup python neuralNetwork2.py > log2.txt &
+nohup python neuralNetwork3.py > log3.txt &
+nohup python neuralNetwork4.py > log4.txt &
+nohup python neuralNetwork5.py > log5.txt &
+nohup python neuralNetwork6.py > log6.txt &
+nohup python neuralNetwork7.py > log7.txt &
+nohup python neuralNetwork8.py > log8.txt &
+nohup python neuralNetwork9.py > log9.txt &
+nohup python neuralNetwork10.py > log10.txt &
+nohup python neuralNetwork11.py > log11.txt &
+nohup python neuralNetwork12.py > log12.txt &
+nohup python neuralNetwork13.py > log13.txt &
+nohup python neuralNetwork14.py > log14.txt &
+nohup python neuralNetwork15.py > log15.txt &
+nohup python neuralNetwork16.py > log16.txt &
+nohup python neuralNetwork17.py > log17.txt &
+nohup python neuralNetwork18.py > log18.txt &
+nohup python neuralNetwork19.py > log19.txt &
+nohup python neuralNetwork20.py > log20.txt &
+nohup python neuralNetwork21.py > log21.txt &
+nohup python neuralNetwork22.py > log22.txt &
+nohup python neuralNetwork23.py > log23.txt &
