0f2be9cd53310a9a5bdd3b1be9c3313ab05d5bf7
Matheus Trevisan Moreira <matheus.trev@gmail.com>
2019-12-30 11:29:13 -0800

    Adding data cleaner script

---
+++ data/cleanCSV.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+perl -0777 -i.original -pe 's/\n([RF])/@\1/g' $1
+sed -i 's#^Path[ ]\+[0-9]\+,\([^,]\+\,[^,]\+\),#Path \1 #g' $1
+awk '!seen[$2]++' $1 > unique_$1
+mv unique_$1 $1
+sed -i 's#Path[ ]\+\([^ ]\+\)[ ]\+#Path 0,\1,#g ; s#@#\n#g' $1
